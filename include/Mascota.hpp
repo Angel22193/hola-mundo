d0f67d44a99d0391fdcd56171afd4c0f1c8ff8d3
Angel22193 <160290098+Angel22193@users.noreply.github.com>
2024-02-20 01:56:42 +0000

    Alimento.cpp

---
+++ include/Mascota.hpp
@@ -0,0 +1,15 @@
+class Mascota
+{
+private:
+   int Felicidad;
+   int Energia;
+public:
+    Mascota() {}
+    ~Mascota() {}
+    void Comer(){
+        this->Energia +=1;
+    }
+    int LeerEnergia(){
+        return this->Energia;
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
 using namespace std;
+#include <Mascota.hpp>
 int main(int argc, char const *argv[])
 {
-    
-    cout<< "Hola "<<"22110324"<<endl;
+    Mascota m1;
+    Mascota m2;
+    Mascota m3;
+
+    m1.Comer();
+    m2.comer();
+    m3.comer();
+
+    cout<< " Mascota "<<m1.leer energia()<<endl;
     return 0;
 }
