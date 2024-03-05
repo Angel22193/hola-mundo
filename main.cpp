db831c9202fd8ce6b6ace62f5a13efcb60f86e7c
Angel22193 <160290098+Angel22193@users.noreply.github.com>
2024-03-05 01:49:01 +0000

    version 5

--- main.cpp
+++ main.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 #include <Mascota.hpp>
 #include <Alimento.hpp>
+#include <Dibujo.hpp>
+
 int main(int argc, char const *argv[])
 {
+    Dibujo dibujo("./assets/kokoa.txt");
+
+    dibujo.Dibujar();
     Mascota m1;
     Mascota m2;
     Mascota m3;
